69fdb04302bf014971d35ad67887f0afcc5b85ef
蒋汪洋 <402233792@qq.com>
2025-09-20 18:36:04 +0800

    调整ResponseBodyEmitter和SseEmitter发送的数据结构

--- src/main/java/com/github/jiangwangyang/demo/controller/ResponseBodyEmitterController.java
+++ src/main/java/com/github/jiangwangyang/demo/controller/ResponseBodyEmitterController.java
@@ -3,39 +3,54 @@ package com.github.jiangwangyang.demo.controller;
 import com.github.jiangwangyang.demo.common.ResponseBodyEmitterWrapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
+import java.nio.charset.StandardCharsets;
+
+/**
+ * SSE适合用来发送结构数据
+ * Emitter适合用于发送无结构数据
+ */
 @RestController
 @Slf4j
 public class ResponseBodyEmitterController {
 
     @GetMapping("/emitter")
-    public ResponseBodyEmitter emitterEmitter(HttpServletResponse response) {
-        response.setContentType("text/stream;charset=utf-8");
+    public ResponseBodyEmitter emitter(HttpServletResponse response) {
+        response.setContentType(MediaType.TEXT_EVENT_STREAM_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         ResponseBodyEmitterWrapper emitter = new ResponseBodyEmitterWrapper(100L);
         new Thread(() -> {
-            emitter.send("你好");
-            emitter.send("你好");
+            emitter.send("你");
+            emitter.send("好");
+            emitter.complete();
+            emitter.send("你");
+            emitter.send("好");
             emitter.complete();
             emitter.complete();
+            emitter.completeWithError(new RuntimeException("emitter异常"));
+            emitter.completeWithError(new RuntimeException("emitter异常"));
         }).start();
         return emitter;
     }
 
     @GetMapping("/emitter/timeout")
     public ResponseBodyEmitter emitterTimeout(HttpServletResponse response) {
-        response.setContentType("text/stream;charset=utf-8");
+        response.setContentType(MediaType.TEXT_EVENT_STREAM_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         ResponseBodyEmitterWrapper emitter = new ResponseBodyEmitterWrapper(100L);
         new Thread(() -> {
-            emitter.send("你好");
+            emitter.send("你");
+            emitter.send("好");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ignored) {
             }
-            emitter.send("你好");
-            emitter.send("你好");
+            emitter.send("你");
+            emitter.send("好");
             emitter.complete();
             emitter.complete();
             emitter.completeWithError(new RuntimeException("emitter异常"));
@@ -46,13 +61,15 @@ public class ResponseBodyEmitterController {
 
     @GetMapping("/emitter/error")
     public ResponseBodyEmitter emitterError(HttpServletResponse response) {
-        response.setContentType("text/stream;charset=utf-8");
+        response.setContentType(MediaType.TEXT_EVENT_STREAM_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         ResponseBodyEmitterWrapper emitter = new ResponseBodyEmitterWrapper(100L);
         new Thread(() -> {
-            emitter.send("你好");
+            emitter.send("你");
+            emitter.send("好");
             emitter.completeWithError(new RuntimeException("emitter异常"));
-            emitter.send("你好");
-            emitter.send("你好");
+            emitter.send("你");
+            emitter.send("好");
             emitter.complete();
             emitter.complete();
             emitter.completeWithError(new RuntimeException("emitter异常"));
--- src/main/java/com/github/jiangwangyang/demo/controller/SseEmitterController.java
+++ src/main/java/com/github/jiangwangyang/demo/controller/SseEmitterController.java
@@ -3,9 +3,13 @@ package com.github.jiangwangyang.demo.controller;
 import com.github.jiangwangyang.demo.common.SseEmitterWrapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -13,29 +17,35 @@ public class SseEmitterController {
 
     @GetMapping("/sse")
     public SseEmitter sseEmitter(HttpServletResponse response) {
-        response.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         SseEmitterWrapper sse = new SseEmitterWrapper(100L);
         new Thread(() -> {
-            sse.send("你好");
-            sse.send("你好");
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
+            sse.complete();
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
             sse.complete();
             sse.complete();
+            sse.completeWithError(new RuntimeException("sse异常"));
+            sse.completeWithError(new RuntimeException("sse异常"));
         }).start();
         return sse;
     }
 
     @GetMapping("/sse/timeout")
     public SseEmitter sseTimeout(HttpServletResponse response) {
-        response.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         SseEmitterWrapper sse = new SseEmitterWrapper(100L);
         new Thread(() -> {
-            sse.send("你好");
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ignored) {
             }
-            sse.send("你好");
-            sse.send("你好");
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
             sse.complete();
             sse.complete();
             sse.completeWithError(new RuntimeException("sse异常"));
@@ -46,13 +56,14 @@ public class SseEmitterController {
 
     @GetMapping("/sse/error")
     public SseEmitter sseError(HttpServletResponse response) {
-        response.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         SseEmitterWrapper sse = new SseEmitterWrapper(100L);
         new Thread(() -> {
-            sse.send("你好");
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
             sse.completeWithError(new RuntimeException("sse异常"));
-            sse.send("你好");
-            sse.send("你好");
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
+            sse.send(Map.of("data", "你好"), MediaType.APPLICATION_JSON);
             sse.complete();
             sse.complete();
             sse.completeWithError(new RuntimeException("sse异常"));
